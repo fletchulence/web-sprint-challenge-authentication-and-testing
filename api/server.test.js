da045ec2839d64ddabd3e36d65e4a6c281a89f1b
david fletcher <davidfletcheraaron@gmail.com>
2022-01-29 16:47:32 -0500

    passing first couple tests

--- api/server.test.js
+++ api/server.test.js
@@ -4,8 +4,9 @@ const db = require('./../data/dbConfig');
 // imports for the rest
 const server = require('./server');
 const bcrypt = require('bcryptjs');
-// const jwtDecode = require('jwt-decode');
+const jwtDecode = require('jwt-decode');
 const request = require('supertest');
+const { BCRYPT_ROUNDS } = require('./secrets');
 
 
 
@@ -20,7 +21,7 @@ beforeAll(async () => {
   await db.migrate.latest();
 });
 
-// // afterALL is used to exit all things cleanly and destroy what it did before
+// afterALL is used to exit all things cleanly and destroy what it did before
 afterAll(async () => {
   await db.destroy();
 });
@@ -30,22 +31,12 @@ test('NODE_ENV is correct', () => {
 });
 
 describe('server.js', () => {
-  describe('[POST] /api/auth/register', () => {
-    let user = { username: 'billy', password: 1234 };
-    let result;
-    beforeEach(async () => {
-      [result] = await User.getAll();
+  describe('REGISTER -- [POST] /api/auth/register', () => {
+    it('[1] responds with the user that is being connected on register', async () => {
+      await request(server).post('/api/auth/register').send({ username: 'bob', password: '1234' });
+      const [users] = await db('users');
+      expect(users.username).toEqual('bob');
     });
-    test('should add a user to the db with a hashed password', async () => {
-      await User.insert({...user, password: bcrypt.hashSync(user.password, BCRYPT_ROUNDS)});
-      expect(result).toMatchObject({ id: 1, username: "billy" });
-      expect(result.password).not.toMatchObject({ id: 1, password: 1234 });
-    });
-    test('added user should have password hashed', () => {
-      // expect(result.password).toBeDefined();
-      // expect(user.password).not.toBe(1234);
-      expect(result).toMatchObject({ password: '1234' });
-    });
-
+    
   });
 });
